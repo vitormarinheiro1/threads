fa52a9ac64db8006ee0a81da2b6b3ae60343b87d
unknown <vitormarinheiro9@gmail.com>
2024-05-29 09:04:42 -0300

    first commit

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,9 +1,52 @@
-import { Container, DivFoto, DivNome, Header, Logo, Nome, NomePerfil, BotaoThreads, DivNomePerfil, FotoPerfil } from "./styled";
+import React, { useState } from "react";
+
+import {
+    Container,
+    DivFoto,
+    DivNome,
+    Header,
+    Logo,
+    Nome,
+    NomePerfil,
+    BotaoThreads,
+    DivNomePerfil,
+    FotoPerfil,
+    TextoBiografia,
+    Biografia,
+    ContainerSeguidoresLink,
+    DivSeguidoresLink,
+    DivIconesInstagram,
+    TextoCustomizavel,
+    Icone,
+    ContainerThreadsOuRespostas,
+    DivThreadsOuRespostas,
+    ContainerThreads,
+    DivFotoThreadsContainer,
+    DivPostThreadsContainer,
+    FotoThreadsDiv,
+    HeaderDivPostThreadsContainer,
+    PerfilThreads,
+    TempoPostThreads,
+    DivPostThreads,
+    DivIconesThreads,
+    ContainerDownload,
+    BotaoBaixarThreads
+} from "./styled";
 
 import LogoSVG from "../../assets/images/logo.svg"
+import LogoInstagramSVG from "../../assets/images/instagram.svg"
+import MaisSVG from "../../assets/images/ellipsis-circle.svg"
+import EllipsesSVG from "../../assets/images/ellipsis.svg"
+import CurtirSVG from "../../assets/images/curtir.svg"
+import ComentariosSVG from "../../assets/images/comentarios.svg"
+import RepostSVG from "../../assets/images/repost.svg"
+import SendSVG from "../../assets/images/send.svg"
 import Perfil from "../../assets/images/me.jpg"
 
 const Profile = () => {
+
+    const [ThreadsOuRespostas, setThreadsOuRespostas] = useState("threads");
+
     return (
         <Container>
             <Logo src={LogoSVG} alt="teste" />
@@ -22,6 +65,103 @@ const Profile = () => {
                     <FotoPerfil src={Perfil} alt="Me"></FotoPerfil>
                 </DivFoto>
             </Header>
+
+            <Biografia>
+                <TextoBiografia>
+                    🕐 Programador sênior há 7 anos.
+                </TextoBiografia>
+                <TextoBiografia>
+                    🥇 Itaú - BTG Pactual - Wiser Educação
+                </TextoBiografia>
+                <TextoBiografia>
+                    🧠 Criação do Framework CSMR
+                </TextoBiografia>
+                <TextoBiografia>
+                    🔥 Minha escola de programação
+                </TextoBiografia>
+            </Biografia>
+
+            <ContainerSeguidoresLink>
+                <DivSeguidoresLink>
+                    <TextoCustomizavel color="#616161" size="14.5">
+                        400 seguidores . onovoprogramador.com
+                    </TextoCustomizavel>
+                </DivSeguidoresLink>
+
+                <DivIconesInstagram>
+                    <Icone src={LogoInstagramSVG}></Icone>
+                    <Icone src={MaisSVG}></Icone>
+                </DivIconesInstagram>
+            </ContainerSeguidoresLink>
+
+            <ContainerThreadsOuRespostas>
+                <DivThreadsOuRespostas onClick={() => { setThreadsOuRespostas("threads") }} outlined={ThreadsOuRespostas === "threads" ? "true" : "false"}>
+                    <TextoCustomizavel color="white" size="15">
+                        Threads
+                    </TextoCustomizavel>
+                </DivThreadsOuRespostas>
+                <DivThreadsOuRespostas onClick={() => { setThreadsOuRespostas("respostas") }} outlined={ThreadsOuRespostas === "respostas" ? "true" : "false"}>
+                    <TextoCustomizavel color="white" size="15">
+                        Respostas
+                    </TextoCustomizavel>
+                </DivThreadsOuRespostas>
+            </ContainerThreadsOuRespostas>
+
+            <ContainerThreads display={ThreadsOuRespostas === "threads" ? "flex" : "none"}>
+                <DivFotoThreadsContainer>
+                    <FotoThreadsDiv src={Perfil} />
+                </DivFotoThreadsContainer>
+
+                <DivPostThreadsContainer>
+                    <HeaderDivPostThreadsContainer>
+                        <PerfilThreads>
+                            <TextoCustomizavel color="white" size="14">
+                                marinheiroz
+                            </TextoCustomizavel>
+                        </PerfilThreads>
+                        <TempoPostThreads>
+                            <TextoCustomizavel size="14" color="grey">
+                                3sem <Icone bottom="-4" src={EllipsesSVG} width="18" />
+                            </TextoCustomizavel>
+                        </TempoPostThreads>
+                    </HeaderDivPostThreadsContainer>
+
+                    <DivPostThreads>
+                        <TextoCustomizavel size="15" top="15" color="white">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        </TextoCustomizavel>
+
+                        <TextoCustomizavel size="15" top="15" color="white">
+                            Fusce nec justo id tortor facilisis fermentum. Integer vitae vestibulum ante.
+                        </TextoCustomizavel>
+
+                        <TextoCustomizavel size="15" top="15" color="white">
+                            Sed pharetra convallis sapien, nec congue dolor aliquam in. Nulla ultrices nulla vitae velit iaculis malesuada.
+                        </TextoCustomizavel>
+                    </DivPostThreads>
+
+                    <DivIconesThreads>
+                        <Icone src={CurtirSVG} width="20" />
+                        <Icone src={ComentariosSVG} width="25" />
+                        <Icone src={RepostSVG} width="22" />
+                        <Icone src={SendSVG} width="20" />
+                    </DivIconesThreads>
+
+                    <TextoCustomizavel size="15" color="grey" top="5">9 curtidas</TextoCustomizavel>
+                </DivPostThreadsContainer>
+            </ContainerThreads>
+
+            <ContainerDownload>
+                <Icone src={LogoSVG} width="42" left="-40"  />
+                <Icone src={Perfil} width="55" left="20" top="-28" border="5px solid #0A0A0A" radius="50%"  />
+                <TextoCustomizavel size="14" top="15" color="#5D5D5D">
+                    Baixe o app Threads para ver mais de marinheiroz.
+                </TextoCustomizavel>
+
+                <BotaoBaixarThreads>
+                    Baixar o Threads
+                </BotaoBaixarThreads>
+            </ContainerDownload>
         </Container>
     );
 }
--- src/pages/profile/styled.ts
+++ src/pages/profile/styled.ts
@@ -1,5 +1,30 @@
 import styled, { createGlobalStyle } from "styled-components";
 
+type TypeTextoCustomizavel = {
+    color: string;
+    size: string;
+    left?: string;
+    top?: string;
+    weigth?: string;
+}
+
+type TypeIcone = {
+    width?: string;
+    bottom?: string;
+    left?: string;
+    top?: string;
+    radius?: string;
+    border?: string;
+}
+
+type TypeDivThreadsOuRespostas = {
+    outlined: string
+}
+
+type TypeContainerThreads = {
+    display?: string;
+}
+
 export const GlobalStyles = createGlobalStyle`
     body {
         margin: 0;
@@ -12,7 +37,7 @@ export const GlobalStyles = createGlobalStyle`
 
 export const Container = styled.div`
     display: flex;
-    width: 610px;
+    width: 630px;
     flex-direction: column;
     align-items: center;
     padding-top: 27px;
@@ -36,6 +61,7 @@ export const DivNome = styled.div`
     width: 100%;
     flex-direction: column;
     justify-content: center;
+    margin-top: -5px;
 `;
 
 export const DivNomePerfil = styled.div`
@@ -60,6 +86,7 @@ export const Nome = styled.h1`
     font-family: 'Roboto', sans-serif;
     font-weight: 500;
     text-decoration: none;
+    letter-spacing: 0.5px;
     margin-top: 5px;
     margin: 0;
     border: 0;
@@ -70,6 +97,7 @@ export const NomePerfil = styled.h2`
     color: #F3F5F7;
     font-family: 'Roboto', sans-serif;
     font-weight: 300;
+    letter-spacing: 0.5px;
     text-decoration: none;
     margin: 0;
     border: 0;
@@ -79,7 +107,7 @@ export const BotaoThreads = styled.button`
     padding: 5px 7px;
     border-radius: 15px;
     font-family: 'Roboto', sans-serif;
-    background-color: #3A3A3A;
+    background-color: #1E1E1E;
     border: 0;
     color: #616161;
     font-size: 10px;
@@ -91,4 +119,173 @@ export const FotoPerfil = styled.img`
     border-radius: 50%;
     width: 100%;
     max-width: 85px;
+`;
+
+export const Biografia = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    width: 100%;
+    margin-top: 15px;
+`;
+
+export const TextoBiografia = styled.h3`
+    border: 0;
+    margin: 0;
+    font-family: 'Roboto', sans-serif;
+    font-size: 15px;
+    letter-spacing: 0.5px;
+    color: #F3F5F7;
+    font-weight: 300;
+    margin-bottom: -3px;
+`;
+
+export const ContainerSeguidoresLink = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    margin-top: 25px;
+`;
+
+export const DivSeguidoresLink = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`;
+
+export const DivIconesInstagram = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-end;
+    flex: 1;
+`;
+
+export const TextoCustomizavel = styled.p<TypeTextoCustomizavel>`
+    border: 0;
+    margin: 0;
+    font-family: 'Roboto', sans-serif;
+    letter-spacing: 0.2px;
+    color: ${(props) => (props.color ? props.color : "white")};
+    font-size: ${(props) => (props.size ? `${props.size}px` : "15px")};
+    margin-left: ${(props) => (props.left ? `${props.left}px` : "0px")};
+    margin-top: ${(props) => (props.top ? `${props.top}px` : "0px")};
+    font-weight: ${(props) => (props.weigth ? props.weigth : 300)};
+`;
+
+export const Icone = styled.img<TypeIcone>`
+    width: 100%;
+    border-radius: ${(props) => props.radius ? props.radius : "0px"};
+    border: ${(props) => props.border ? props.border : "0px"};
+    max-width: ${(props) => props.width ? `${props.width}px` : "23px"};
+    margin-right: 15px;
+    margin-bottom: ${(props) => props.bottom ? `${props.bottom}px` : "0px"};
+    margin-left: ${(props) => props.left ? `${props.left}px` : "0px"};
+    margin-top: ${(props) => props.top ? `${props.top}px` : "0px"};
+`;
+
+export const ContainerThreadsOuRespostas = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+`;
+
+export const DivThreadsOuRespostas = styled.div<TypeDivThreadsOuRespostas>`
+    display: flex;
+    flex: 1;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 12px 0px;
+    margin-top: 15px;
+    border-bottom: ${(props) => props.outlined === "true" ? "1px solid white" : "0px"};
+    cursor: pointer;
+`;
+
+export const ContainerThreads = styled.div<TypeContainerThreads>`
+    display: ${(props) => props.display ? props.display : "block"};
+    color: white;
+    width: 100%;
+    padding-top: 30px;
+`;
+
+export const DivFotoThreadsContainer = styled.div`
+    display: flex;
+    flex: 1;
+`;
+
+export const FotoThreadsDiv = styled.img`
+    border-radius: 50%;
+    width: 100%;
+    max-width: 37px;
+    max-height: 37px;
+`;
+
+export const DivPostThreadsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex: 8;
+    width: 100%;
+    margin-left: -25px;
+`;
+
+export const HeaderDivPostThreadsContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+`;
+
+export const PerfilThreads = styled.div`
+    display: flex;
+    flex: 1;
+    cursor: pointer;
+
+`;
+
+export const TempoPostThreads = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    flex: 1;
+`;
+
+export const DivPostThreads = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+`;
+
+export const DivIconesThreads = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 5px 0px;
+    margin-top: 15px;
+`;
+
+export const ContainerDownload = styled.div`
+    margin-top: 35px;
+    background-color: #0A0A0A;
+    width: 100%;
+    max-width: 500px;
+    border-radius: 15px;
+    height: 150px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 60px;
+`;
+
+export const BotaoBaixarThreads = styled.button`
+    font-family: 'Roboto', sans-serif;
+    font-size: 15px;
+    color: #F3F5F7;
+    border: 1.5px solid #5D5D5D;
+    background-color: transparent;
+    padding: 7px 12px;
+    border-radius: 10px;
+    font-weight: 300px;
+    margin-top: 30px;
 `;
